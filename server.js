5e51e9f8083102ab817a9ad4cd0bce55ad60e2c9
kjozwiak <kamiljoz@gmail.com>
2016-01-29 15:19:37 -0500

    using the js standard style

--- server.js
+++ server.js
@@ -1,64 +1,63 @@
-'use strict';
+'use strict'
 
-const Hapi = require('hapi');
-const Blankie = require('blankie');
-const Scooter = require('scooter');
-const server = new Hapi.Server();
+const Hapi = require('hapi')
+const Blankie = require('blankie')
+const Scooter = require('scooter')
+const server = new Hapi.Server()
 
-server.connection({ port: 22935});
+server.connection({ port: 22935 })
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Coming soon....... :)');
-    }
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Coming soon....... :)')
+  }
 })
 
 server.register(require('inert'), (err) => {
-    if (err) {
-        throw err;
+  if (err) {
+    throw err
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './'
+      }
     }
+  })
 
-    server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-            path: './'
-            }
+  server.route({
+    method: 'GET',
+    path: '/experiment/{param*}',
+    config: {
+      handler: {
+        directory: {
+          path: 'experiment'
         }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/experiment/{param*}',
-        config: {
-            handler: {
-                directory: {
-                path: 'experiment'
-                }
-            },
-            plugins: {
-                blankie: false
-            }
-        }
-    });
-});
+      },
+      plugins: {
+        blankie: false
+      }
+    }
+  })
+})
 
 server.register([Scooter, {
-    register: Blankie,
-    options: {
-        // examples of CSP directives that affect the entire server
-        // objectSrc: ['unsafe-inline', 'unsafe-eval']
-    }
+  register: Blankie,
+  options: {
+    // examples of CSP directives that affect the entire server
+    // objectSrc: ['unsafe-inline', 'unsafe-eval']
+  }
 }], function (err) {
+  if (err) {
+    throw err
+  }
 
-    if (err) {
-        throw err;
-    }
-
-    server.start(() => {
-        console.log('Server running at:', server.info.uri);
-    });
-});
+  server.start(() => {
+    console.log('Server running at:', server.info.uri)
+  })
+})
